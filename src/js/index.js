30b5462dad2c625ee4a9d692cb26d35fff1eb571
Egor Badulin <nobodykenny@gmail.com>
2022-06-30 03:50:53 +0500

    add: margin/padding classes

--- src/js/index.js
+++ src/js/index.js
@@ -1,8 +1,7 @@
 import "normalize.css";
 import "../css/main.scss";
-import symbol from '../img/sprite/icon-camera.svg';
+import '../img/sprite/icon-camera.svg';
 
 document.addEventListener('DOMContentLoaded', () => {
-    console.log('it is work');
-    console.log(symbol);
+    
 });
