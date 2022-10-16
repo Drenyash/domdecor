17668bae7622196831733434f3e3b811f791ae4b
Egor Badulin <nobodykenny@gmail.com>
2022-10-17 02:45:52 +0300

    feat: add temp section

--- src/js/pages.js
+++ src/js/pages.js
@@ -6,6 +6,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const head = fs.readFileSync('src/includes/head.html');
 const sectionHeader = fs.readFileSync('src/includes/section-header.html');
 const sectionFooter = fs.readFileSync('src/includes/section-footer.html');
+const temp = fs.readFileSync('src/includes/temp.html');
 
 // Pages
 module.exports = [
@@ -16,7 +17,8 @@ module.exports = [
         title: "Dranik",
         head,
         sectionHeader,
-        sectionFooter
+        sectionFooter,
+        temp
     }),
     new HtmlWebpackPlugin({
         template: "./src/pages/page.html",
@@ -25,6 +27,7 @@ module.exports = [
         title: "Title child page | Dranik",
         head,
         sectionHeader,
-        sectionFooter
+        sectionFooter,
+        temp
     })
 ];
